71bf8babd08b31b7ef11b0b5b098de50e675ddcb
Remy <jmgwitlox@hotmail.com>
2020-06-11 18:11:31 +0200

    .

--- loginService/src/main/java/dreambike/Controller/GatewayController.java
+++ loginService/src/main/java/dreambike/Controller/GatewayController.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,24 +23,14 @@ public class GatewayController {
 	@Autowired
 	DockingStationService dockingstationService;
 	
-	@GetMapping("test")
-	public String test() {
-		return "test";
-	}
-	
-	@GetMapping("test2")
-	public String test2() {
-		return bikeService.getTest();
-	}
-	
-	@GetMapping("getAllDocking")
-	public String getAllDocking() throws IOException {
+	@GetMapping("getAll")
+	public String getAll() throws IOException {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime now = LocalDateTime.now();  
 		System.out.println(dtf.format(now) + " getAllDocking called.");
 		return dockingstationService.getAll();
 	}
-	
+
 	@GetMapping("getOneDocking")
 	public DockingStation getOneDocking() {
 		return dockingstationService.getOne();
